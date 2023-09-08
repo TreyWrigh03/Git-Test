ca569fcff865a3a9a29874d72dd805259457129f
Trey Wright <dwrigh43@ut.utm.edu>
2023-09-07 21:35:07 -0500

    Finished up the product function

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@ int sum(int n){
   }
   return numbies;
 }
+
+int mult(int n){
+  int multies = 1;
+  for(int i =1; i <= n; i++){
+    multies = multies * i;
+  }
+  return multies;
+}
+
+
 int main() {
 
   int input;
@@ -17,6 +27,7 @@ int main() {
   cin >> input;
   
   cout << sum(input) << "\n";
+  cout << mult(input) << "\n";
   
   return 0;
 }
